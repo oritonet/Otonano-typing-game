33466074e2f45093fa0899b998cf79ce11b005cb
Taro <o47638708@gmail.com>
2025-12-14 20:17:08 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -438,9 +438,9 @@ engine.attach();
 /* =========================
    Paste/Drop 禁止
 ========================= */
-inputEl.addEventListener("paste", (e) => e.preventDefault());
-inputEl.addEventListener("drop", (e) => e.preventDefault());
-inputEl.addEventListener("dragover", (e) => e.preventDefault());
+/inputEl.addEventListener("paste", (e) => e.preventDefault());
+/inputEl.addEventListener("drop", (e) => e.preventDefault());
+/inputEl.addEventListener("dragover", (e) => e.preventDefault());
 
 /* =========================
    Countdown + Start
@@ -901,3 +901,4 @@ onAuthStateChanged(auth, async (user) => {
   await init();
   await loadMyAnalytics(user.uid, userMgr.getCurrentUserName());
 });
+
